9c40138089006688dfc8e6a3fb0f540cfffef316
swl <swl1314521>
2019-09-09 15:57:03 +0800

    生成profile zip 并下载

--- src/main/java/com/jf/jf_smartsite/gen/controller/EqGenController.java
+++ src/main/java/com/jf/jf_smartsite/gen/controller/EqGenController.java
@@ -1,15 +1,14 @@
 package com.jf.jf_smartsite.gen.controller;
 
 
-
 import com.jf.jf_smartsite.gen.service.EqGenTableService;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.io.*;
 
 
 /**
@@ -19,8 +18,7 @@ import java.io.IOException;
  */
 @Controller
 @RequestMapping("/equipment/gen")
-public class EqGenController
-{
+public class EqGenController {
     private String prefix = "equip/gen";
 
     @Autowired
@@ -30,21 +28,45 @@ public class EqGenController
      * 生成profile 产品原型
      */
     @RequestMapping("/GenFile")
-    public void batchGenCode(HttpServletResponse response,int id) throws IOException
-    {
-        byte[] data =   eqGenTableService.selectGenTable(id);
-        genCode(response, data);
+    public void batchGenCode(HttpServletResponse response, int id)  {
+        eqGenTableService.selectGenTable(id);
+        genCode(response);
     }
 
     /**
      * 生成zip文件
      */
-    private void genCode(HttpServletResponse response, byte[] data) throws IOException
-    {
-        response.reset();
-        response.setHeader("Content-Disposition", "attachment; filename=\"profile.zip\"");
-        response.addHeader("Content-Length", "" + data.length);
-        response.setContentType("application/octet-stream; charset=UTF-8");
-        IOUtils.write(data, response.getOutputStream());
+    private void genCode(HttpServletResponse res)  {
+        res.setHeader("content-type", "application/octet-stream");
+        res.setContentType("application/octet-stream");
+        res.setHeader("Content-Disposition", "attachment; filename=SmartSiteGateWay");
+        byte[] buff = new byte[1024];
+        BufferedInputStream bis = null;
+        OutputStream os = null;
+
+        try {
+            os = res.getOutputStream();
+            bis = new BufferedInputStream(new FileInputStream(
+                    new File("./local/SmartSiteGateWay.zip")));
+            int i = bis.read(buff);
+
+            while (i != -1) {
+                os.write(buff, 0, buff.length);
+                os.flush();
+                i = bis.read(buff);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bis != null) {
+                try {
+                    bis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        System.out.println("export file finish");
     }
+
 }
--- src/main/java/com/jf/jf_smartsite/gen/service/EqGemTableServiceImpl.java
+++ src/main/java/com/jf/jf_smartsite/gen/service/EqGemTableServiceImpl.java
@@ -7,7 +7,6 @@ import com.jf.jf_smartsite.gen.domain.ServiceTypeCapabilitie;
 import com.jf.jf_smartsite.gen.domain.ServiceTypeCapabilities;
 import com.jf.jf_smartsite.gen.mapper.EqGenTableMapper;
 import com.jf.jf_smartsite.gen.util.JsonFormatTool;
-import com.jf.jf_smartsite.gen.util.ZipUtils;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -25,7 +23,7 @@ import java.util.zip.ZipOutputStream;
 @Service
 public class EqGemTableServiceImpl implements EqGenTableService {
     private static final Logger log = LoggerFactory.getLogger(EqGemTableServiceImpl.class);
-    private static StringBuffer filePath = new StringBuffer("local/SmartSiteGateWay/");
+    private static StringBuffer filePath = new StringBuffer("./local/SmartSiteGateWay/");
     private String fileName = "devicetype-capability";
     /**
      * 查询设备原型信息
@@ -39,6 +37,16 @@ public class EqGemTableServiceImpl implements EqGenTableService {
     public byte[] selectGenTable(int id) {
         deleteFile(new File(filePath.toString()));
         genDevice(id);
+        String sourceFilePath = "./local/";
+        String zipFilePath = "./local/";
+        String fileName = "SmartSiteGateWay";
+        boolean flag = fileToZip(sourceFilePath, zipFilePath, fileName);
+        if(flag){
+            System.out.println("文件打包成功!");
+        }else{
+            System.out.println("文件打包失败!");
+        }
+
         return zip();
     }
 
@@ -173,5 +181,73 @@ public class EqGemTableServiceImpl implements EqGenTableService {
 
         // 返回是否成功的标记
         return flag;
+    }
+
+
+    /**
+     * 将存放在sourceFilePath目录下的源文件，打包成fileName名称的zip文件，并存放到zipFilePath路径下
+     *
+     * @param sourceFilePath :待压缩的文件路径
+     * @param zipFilePath    :压缩后存放路径
+     * @param fileName       :压缩后文件的名称
+     * @return
+     */
+    public boolean fileToZip(String sourceFilePath, String zipFilePath, String fileName) {
+        boolean flag = false;
+        File sourceFile = new File(sourceFilePath);
+        FileInputStream fis = null;
+        BufferedInputStream bis = null;
+        FileOutputStream fos = null;
+        ZipOutputStream zos = null;
+
+        if (sourceFile.exists() == false) {
+            System.out.println("待压缩的文件目录：" + sourceFilePath + "不存在.");
+        } else {
+            try {
+                File zipFile = new File(zipFilePath + "/" + fileName + ".zip");
+                if (zipFile.exists()) {
+                    System.out.println(zipFilePath + "目录下存在名字为:" + fileName + ".zip" + "打包文件.");
+                } else {
+                    File[] sourceFiles = sourceFile.listFiles();
+                    if (null == sourceFiles || sourceFiles.length < 1) {
+                        System.out.println("待压缩的文件目录：" + sourceFilePath + "里面不存在文件，无需压缩.");
+                    } else {
+                        fos = new FileOutputStream(zipFile);
+                        zos = new ZipOutputStream(new BufferedOutputStream(fos));
+                        byte[] bufs = new byte[1024 * 10];
+                        for (int i = 0; i < sourceFiles.length; i++) {
+                            //创建ZIP实体，并添加进压缩包
+                            ZipEntry zipEntry = new ZipEntry(sourceFiles[i].getName());
+                            zos.putNextEntry(zipEntry);
+                            //读取待压缩的文件并写进压缩包里
+                            fis = new FileInputStream(sourceFiles[i]);
+                            bis = new BufferedInputStream(fis, 1024 * 10);
+                            int read = 0;
+                            while ((read = bis.read(bufs, 0, 1024 * 10)) != -1) {
+                                zos.write(bufs, 0, read);
+                            }
+                        }
+                        flag = true;
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            } finally {
+                //关闭流
+                try {
+                    if (null != bis) bis.close();
+                    if (null != zos) zos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        return flag;
+
     }
 }
